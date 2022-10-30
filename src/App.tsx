daccd492022b1958ba877622b47d007029ad0771
kota-es <info@k-3110.com>
2022-10-30 20:26:11 +0900

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Page1 from "./pages/Page1";
+import Page2 from "./pages/Page2";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="page1" element={<Page1 />} />
+        <Route path="page2" element={<Page2 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
